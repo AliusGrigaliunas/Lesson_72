1284fcd3dbb7a16d60659d397d67b6a45fe3065f
AliusGrigaliunas <Alius.grigaliunas@gmail.com>
2022-08-12 13:25:08 +0300

    feat(72): 2nd task 50% completed

--- src/main.ts
+++ src/main.ts
@@ -42,6 +42,11 @@
 */
 
 console.group('1. Sukurkite klasę tėvinę Person vaikinėms klasėms ir išsaugokite joje bendrą funkcionalumą.');
+type PersonProps = {
+  name: string,
+  surname: string
+};
+
 {
   class Person {
     private static count: number = 0;
@@ -52,7 +57,7 @@ console.group('1. Sukurkite klasę tėvinę Person vaikinėms klasėms ir išsau
 
     public readonly id: string;
 
-    constructor(name:string, surname:string) {
+    constructor(name: string, surname: string) {
       Person.count += 1;
       this.id = `Person_${Person.count}`;
       this.setName(name);
@@ -67,12 +72,29 @@ console.group('1. Sukurkite klasę tėvinę Person vaikinėms klasėms ir išsau
       this.surname = value;
     }
 
-    public getFullName() {
-      return `${this.name} ${this.surname} ${this.id}}`;
+    public get FullName() {
+      return `${this.name} ${this.surname}`;
     }
   }
 
   class Student extends Person {
+    private marks: number[];
+
+    constructor(name:string, surname:string) {
+      super(name, surname);
+      this.marks = [];
+    }
+
+    public addMark(mark: number) {
+      this.marks.push(mark);
+    }
+
+    public getAvg() {
+      return this.marks.reduce((
+        currentValue: number,
+        previousValue: number,
+      ) => (currentValue + previousValue)) / this.marks.length;
+    }
   }
 
   class Lecturer extends Person {
@@ -85,6 +107,20 @@ console.group('1. Sukurkite klasę tėvinę Person vaikinėms klasėms ir išsau
     private static PSD_PERC = 0.0698;
 
     private static VSD_PERC = 0.1252;
+
+    private PrivateSalary!:number;
+
+    constructor(salary: number, name: string, surname: string) {
+      super(name, surname);
+      this.PrivateSalary = salary;
+    }
+
+    set salary(value:number) {
+      if (value < Lecturer.MIN_SALARY) throw new Error('Atlyginimas per žemeas');
+      if (value > Lecturer.MAX_SALARY) throw new Error('Atlyginimas per didelis');
+
+      this.PrivateSalary = value;
+    }
   }
 
   const people: Person[] = [
@@ -96,7 +132,7 @@ console.group('1. Sukurkite klasę tėvinę Person vaikinėms klasėms ir išsau
   // 20min
   console.group('1.1. Sukurkite klasę Person, kurios objektams būtų galima priskirti vardą ir pavardę. Šios klasės objektams turi susigeneruoti id - unikalus raktas. Taip pat sukurkite get"erį fullname, kuris grąžina vardą ir pavardę atskirtus tarpu. Atspausdinkite kelis šios klasės objektus, ir pademonstruokite get"erio veikimą.');
   {
-    const peopleFullname = people.map((p) => p.getFullName());
+    const peopleFullname = people.map((p) => p.FullName);
 
     console.log(peopleFullname);
   }
@@ -105,12 +141,40 @@ console.group('1. Sukurkite klasę tėvinę Person vaikinėms klasėms ir išsau
   // 40min
   console.group('1.2. Sukurkite klasę Student, kuri paveldėtų klasę Person. Be Person klasės paveldimų savybių, klasę Student turi turėti savybę "marks", kurioje bus saugomi studento pažymiai. Konstruktoriaus vykdymo metu, "marks" reikšmei turi būti sukuriamas tuščias masyvas. Student klasėje sukurkite metodą "addMark" kuris leistų įdėti naują pažymį - sveiką skaičių nuo 1 iki 10. Taip pat sukurkite get"erį "avg", kuris skaičiuotų studento vidurkį pagal esamus pažymius. Sukurkite bent 2 Student klasės objektus ir atspausdinkite visus get"erius ir pavaizduokite metodų funkcionalumą konsolėje.');
   {
+    const Student1 = new Student('Alius', 'Grigaliūnas');
+    Student1.addMark(6);
+    Student1.addMark(7);
+    Student1.addMark(10);
+
+    Student1.getAvg();
+
+    console.log(Student1);
+    console.log(Student1.getAvg());
+
+    const Student2 = new Student('Juozas', 'Petraitis');
+    Student2.addMark(8);
+    Student2.addMark(6);
+    Student2.addMark(4);
+
+    console.log(Student2);
+    console.log(Student2.getAvg());
   }
   console.groupEnd();
 
   // 50min
   console.group('1.3. Sukurkite klasę Lecturer, kuri paveldėtų klasę Person. Papildomai klasei Lecturer sukurkite savybę "salary", kuri privalo būti priskirta objekto sukūrimo metu. Inkapsuliuokite savybę "salary" taip, kad ji galėtų būti skaičius nuo 1800 iki 4400 su ne daugiau nei 2 skaičiais po kablelio. Taip pat sukurkite get"erį "salaryNeto", kuris atspausdintų suapvalintą atlyginimą iki sveikų skaičių atskaičiavus mokesčius: GPM 20%, PSD 6.98%, VSD 12.52%. Sukurkite bent 2 Lecturer klasės objektus ir atspausdinkite visus get"erius konsolėje.');
   {
+    const lectur1 = new Lecturer('Juozas', 'Petraitis');
+    const lectur2 = new Lecturer('Povilas', 'Smetona');
+
+    const lecturers = [lectur1, lectur2];
+
+    const pay = 1900;
+    const pay2 = 2600;
+
+    console.log(lectur1);
+
+    lecturers.forEach(({ FullName }) => console.log(FullName));
   }
   console.groupEnd();
 
